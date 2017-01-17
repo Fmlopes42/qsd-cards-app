679c29e55311f2d8f71f43e03cbb5c53c734d8a6
Fernanda Monteiro <fmlopes42@gmail.com>
2017-01-17 11:10:08 -0200

    add feature not case sensitive uniqueness

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ApplicationRecord
   validates :frente, :verso, presence: true
-  validates :frente, :verso, uniqueness: true
+  validates :frente, :verso, uniqueness: { case_sensitive: false }
 end
